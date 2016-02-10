11a52d5684faf1d3ccba528f29e44fb5e22fa4f9
Tsvetelina Ivanova <tsvetelina.ivanova@telerik.com>
2016-02-10 13:25:32 +0000

    Changes made by Tsvetelina Ivanova (tsvetelina.ivanova@telerik.com).

--- exp-app/app/app.router.ts
+++ exp-app/app/app.router.ts
@@ -1,7 +1,19 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {DashboardComponent} from './components/dashboard/app.dashboard';
+import {ListExpComponent} from './components/list-exp/app.list-exp';
+import {NewExpComponent} from './components/new-exp/app.new-exp';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Main</h1>'
+    templateUrl: 'app/router.component.html',
+    directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  { path:'/', name: 'Dashboard', component: DashboardComponent },
+  { path:'/list-exp', name: 'ListExp', component: ListExpComponent },
+  { path:'/new-exp', name: 'NewExp', component: NewExpComponent }
+])
+
 export class AppRouter { }
