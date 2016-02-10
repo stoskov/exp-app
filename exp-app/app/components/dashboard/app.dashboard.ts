e4210c55e5883dbff13b585e19948fa20d66b135
Svetozar Toskov <s.toskov@abv.bg>
2016-02-10 11:04:46 +0200

    Add views

--- exp-app/app/components/dashboard/app.dashboard.ts
+++ exp-app/app/components/dashboard/app.dashboard.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App 23</h1>'
+    template: '<h1>Dashboard</h1>'
 })
-export class AppComponent { }
+export class Dashboard { }
